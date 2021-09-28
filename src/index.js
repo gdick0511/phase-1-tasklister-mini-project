7ebaeb9ef292428725378cc1a08b1872617232ba
garrettdick <garrettd0511@gmail.com>
2021-09-28 12:11:42 -0500

    New Changes

--- src/index.js
+++ src/index.js
@@ -1,3 +1,41 @@
+// document.addEventListener("DOMContentLoaded", () => {
+//   // your code here
+//   let form = document.querySelector('form').addEventListener('submit', (e) => {
+//     e.preventDefault()
+//    handleToDo(e.target['new-task-description'].value)
+//   })
+
+// });
+
+// function handleToDo(todo){
+//   let li = document.createElement('li')
+//   li.textContent = todo
+//   document.querySelector('#tasks').appendChild(li)
+// }
+
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
-});
+  function handelFormSubmit(event) {
+    event.preventDefault()
+    
+    
+    let newToDoItem = event.target["new-task-description"].value
+    const li = document.createElement('li')
+    let btn = document.createElement('button')
+    btn.addEventListener('click', handleDelete)
+    btn.textContent = 'x'
+    li.textContent = newToDoItem
+    li.append(btn)
+    let ul = document.getElementById('tasks')
+    ul.append(li)
+    
+    // event.target.reset()
+    form.reset()
+  }
+ 
+  const form = document.getElementById('create-task-form')
+  form.addEventListener('submit',handelFormSubmit)
+})
+
+function handleDelete(e){
+  e.target.parentNode.remove()
+}
